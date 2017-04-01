4bcea4e2058e2d53f5ea9a963f78efc36faae02c
Vahesan Jeyaraj <vahesan@live.com>
2017-04-01 22:06:29 +0530

    Update

--- GSM_CODE.ino
+++ GSM_CODE.ino
@@ -1,10 +1,11 @@
 #include <SoftwareSerial.h>
 SoftwareSerial gsm(10, 11); // RX, TX
-
+String msg; 
+String test;
 void setup()
 {
-	gsm.begin(9600);
 	Serial.begin(9600);
+	gsm.begin(9600);
 	pinMode(13, OUTPUT);
 	digitalWrite(13, LOW);
 	gsmInit();
@@ -12,18 +13,12 @@ void setup()
 
 void loop()
 {
-	if (gsm.available()>0)
+	if (gsm.available() > 0)
 	{
-		Serial.println(gsm.readString());
+		msg = gsm.readString();
 	}
-	gsm.println("AT+CMGF=1");
-	delay(400);
-	gsm.println("AT+CMGS=\"+94779847283\"");   // use your 10 digit cell no. here
-	delay(400);
-	Serial.println("TEST");
-	delay(200);
-	Serial.write(26);
-	delay(10000);
+		Serial.println(msg);
+		
 }
 
 
@@ -48,7 +43,7 @@ void gsmInit()
 	while (echo_flag)
 	{
 		gsm.println("ATE0");
-		while (gsm.available()>0)
+		while (Serial.available()>0)
 		{
 			if (gsm.find("OK"))
 				echo_flag = 0;
@@ -75,6 +70,4 @@ void gsmInit()
 	delay(500);
 	gsm.println("AT+CNMI=2,2,0,0,0");
 	delay(500);
-
-	digitalWrite(13, HIGH);
 }
